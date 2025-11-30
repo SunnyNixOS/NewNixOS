9965bb768c6be19309e1182715fb611155996ff4
Sunny <sun+nxisoftgithub@coinblock25.com>
2025-11-30 09:32:16 -0800

    pathetic attempt at aliases

---
+++ hosts/alias.nix
@@ -0,0 +1,8 @@
+{ pkgs, lib, config, ... }
+{
+
+    environment.shellAliases = {
+        reb = "sudo nixos-rebuild switch --flake /home/jaime/.dotfiles#jaime-nix-gamingdesktop";
+    };
+
+}
--- modules/nvf/main.nix
+++ modules/nvf/main.nix
@@ -3,9 +3,72 @@
 {
     programs.nvf = {
         enable = true;
-        settings = {
-            vim.opt.number = true;
-            vim.opt.relativenumber = false;
+
+        settings.vim = {
+                lsp.enable = true;
+                vimAlias = true;
+                viAlias = true;
+                enableLuaLoader = true;
+                preventJunkFiles = true;
+
+
+                telescope.enable = true;
+                lineNumberMode = "relNumber";
+                options = {
+                    splitbelow = true;
+                    splitright = true;
+                    wrap = false;
+                    tabstop = 4;
+                    shiftwidth = 2;
+                };
+
+
+                clipboard = {
+                    enable = true;
+                    registers = "unnamedplus";
+                    providers = {
+                        wl-copy.enable = true;
+                        xsel.enable = true;
+                        };
+                };
+
+
+                languages = {
+                    enableFormat = true;
+                    enableTreesitter = true;
+
+                    nix.enable = true;
+                    python.enable = true;
+                };
+
+                statusline.lualine = {
+                    enable = true;
+                    theme = "base16";
+                };
+
+                extraPlugins = {
+
+                    yazi = {
+                        package = pkgs.vimPlugins.yazi-nvim;
+                        setup = ''
+                        vim.keymap.set("n", "<leader>-", function()
+                            require("yazi").yazi()
+                        end)
+
+                        vim.g.loaded_netrwPlugin = 1
+
+                        vim.api.nvim_create_autocmd("UIEnter", {
+                            callback = function()
+                                require("yazi").setup({
+                                    open_for_directories = true,
+                                })
+                            end,
+                        })
+                        '';
+                    };
+                };
+
+            };
+
         };
-    };
 }
