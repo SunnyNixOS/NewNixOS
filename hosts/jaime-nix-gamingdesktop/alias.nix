fb6f04d9589129d6e732cd9c794784fee2d34092
Sunny <sun+nxisoftgithub@coinblock25.com>
2025-11-30 09:39:34 -0800

    fixed alias

--- hosts/jaime-nix-gamingdesktop/alias.nix
+++ hosts/jaime-nix-gamingdesktop/alias.nix
@@ -1,8 +1,9 @@
-{ pkgs, lib, config, ... }
+{ pkgs, lib, config, ... }:
 {
 
     environment.shellAliases = {
         reb = "sudo nixos-rebuild switch --flake /home/jaime/.dotfiles#jaime-nix-gamingdesktop";
+        dot = "cd /home/jaime/.dotfiles";
     };
 
 }
