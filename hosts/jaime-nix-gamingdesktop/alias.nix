bbd2ed44e4adae68e5fc525561fb2068281ed49c
Sunny <sun+nxisoftgithub@coinblock25.com>
2025-12-06 11:27:14 -0800

    added streamcontroller icons and cheat menu

--- hosts/jaime-nix-gamingdesktop/alias.nix
+++ hosts/jaime-nix-gamingdesktop/alias.nix
@@ -4,6 +4,7 @@
     environment.shellAliases = {
         reb = "sudo nixos-rebuild switch --flake /home/jaime/.dotfiles#jaime-nix-gamingdesktop";
         dot = "cd /home/jaime/.dotfiles";
+        cheat = "nvim /home/jaime/.dotfiles/manual/cheat";
     };
 
 }
